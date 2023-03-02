5a84082d578d2cdbd93c17d3dfd02d5529b22bf1
estebbins <stebbins.e@gmail.com>
2023-03-02 13:48:21 -0500

    Adds cats model in mainapp

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ App created using django
 - As a User, when I click the View All My Cats link, I want to see a page listing all of my cats
 
 ## Routes
+
+route controller option
+/cats cats index view all cats
+/cats/:id show cats view one cat
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
+# temporary cats for building templates
+cats = [
+    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry lil demon', 'age': 12},
+    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 3},
+    {'name': 'Tubbs', 'breed': 'ragdoll', 'description': 'pretty floppy', 'age': 0},
+]
+
 # Create your views here.
 # View functions match urls to code (like controllers in Express)
 # Define our home view function
 def home(request):
-  # Include an .html file extension - unlike when rendering EJS templates
-  return render(request, 'home.html')
+    # Include an .html file extension - unlike when rendering EJS templates
+    return render(request, 'home.html')
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
+#index route for cats
+def cats_index(request):
+    # Just like we passed data to our templates in express
+    # We pass data to our templates through our view functions
+    return render(request, 'cats/index.html', { 'cats': cats })

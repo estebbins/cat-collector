9566bd89ec03b13e058f28f0d64b94455782c475
estebbins <stebbins.e@gmail.com>
2023-03-03 11:34:25 -0500

    Adds edit & delete functionality for cats

--- README.md
+++ README.md
@@ -3,9 +3,16 @@ App created using django
 
 ## User Stories
 - As a User, when I click the View All My Cats link, I want to see a page listing all of my cats
+- AAU, when I click on a cat in the cat list, I want to see a page that displays all the details for that cat.
 
 ## Routes
 
 route controller option
 /cats cats index view all cats
 /cats/:id show cats view one cat
+
+### Development Steps:
+    1. Start by adding the url to our urls
+    2. Create the view associated with that url
+    3. Make the HTML page
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # temporary cats for building templates
-cats = [
-    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry lil demon', 'age': 12},
-    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 3},
-    {'name': 'Tubbs', 'breed': 'ragdoll', 'description': 'pretty floppy', 'age': 0},
-]
+# cats = [
+#     {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry lil demon', 'age': 12},
+#     {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 3},
+#     {'name': 'Tubbs', 'breed': 'ragdoll', 'description': 'pretty floppy', 'age': 0},
+# ]
 
 # Create your views here.
 # View functions match urls to code (like controllers in Express)
@@ -21,4 +23,32 @@ def about(request):
 def cats_index(request):
     # Just like we passed data to our templates in express
     # We pass data to our templates through our view functions
+    # We can gather relations from SQL using our model methods
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', { 'cats': cats })
+
+#Detail route for cats
+# Cat_id is defined, expecting an integer, in our URL
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+
+    return render(request, 'cats/detail.html', { 'cat': cat })
+
+class CatCreate(CreateView):
+    model = Cat
+    #The fields attribute if required for a create view
+    fields = '__all__'
+    # Same as below =>
+    # fields = ['name', 'breed', 'description', 'age']
+    # success_url = "/cats/{cat_id}"
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # custom fields to disallow renaming a cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    #success url, index page
+    success_url = '/cats/'
+
